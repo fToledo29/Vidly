badd4144e69461cc53fd76869224a38e45d70916
Fernando Toledo Ramirez <fernando_29837@live.com>
2017-05-18 18:59:14 -0500

    API controller was created

---
+++ WebApplication2/DTOs/CustomerDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using VidlyII.Models;
+
+namespace VidlyII.DTOs
+{
+    public class CustomerDto
+
+    {
+        public int Id { get; set; }
+
+        // Overwritten error message
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewstetter { get; set; }
+        
+        public byte MembershipTypeId { get; set; }
+
+        //[Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
